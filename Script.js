1632ebc0c8e179c2058036957d44c38e43b8ff0e
YenneBenson <inyenebenson19@gmail.com>
2023-11-22 23:20:21 +0100

    Initial commit

---
+++ Script.js
@@ -0,0 +1,62 @@
+const change = document.getElementById("View");
+
+change.addEventListener("click", (event) =>{
+    event.target.style.color = "white"
+    event.target.style.background = "pink"
+})
+
+const change2 = document.getElementById("Nature");
+
+change2.addEventListener("click", (event) =>{
+    event.target.style.color = "white"
+    event.target.style.background = "green"
+})
+
+const change3 = document.getElementById("Beach");
+
+change3.addEventListener("click", (event) =>{
+    event.target.style.color = "white"
+    event.target.style.background = "brown"
+})
+
+const change4 = document.getElementById("Towers");
+
+change4.addEventListener("click", (event) =>{
+    event.target.style.color = "white"
+    event.target.style.background = "blue"
+})
+
+const change5 = document.getElementById("View");
+
+change5.addEventListener("click", (event) =>{
+    event.target.style.color = "white"
+    event.target.style.background = "pink"
+})
+
+const change6 = document.getElementById("Mosque");
+
+change6.addEventListener("click", (event) =>{
+    event.target.style.color = "white"
+    event.target.style.background = "black"
+})
+
+const change7 = document.getElementById("Skies");
+
+change7.addEventListener("click", (event) =>{
+    event.target.style.color = "white"
+    event.target.style.background = "brown"
+})
+
+const change8 = document.getElementById("Tents");
+
+change8.addEventListener("click", (event) =>{
+    event.target.style.color = "white"
+    event.target.style.background = "blue"
+})
+
+const change9 = document.getElementById("Pyramids");
+
+change9.addEventListener("click", (event) =>{
+    event.target.style.color = "white"
+    event.target.style.background = "red"
+})
